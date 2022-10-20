55305eba778acba394d7374b04526cb80d3495b8
intania <nui-nui-nui@windowslive.com>
2022-10-20 21:40:55 +0700

    เพิ่มปุ่ม Edit ยังไม่เสร็จในหน้าโหลดข้อมูล User

--- app/src/main/java/com/example/test_login/Admin_edit_user_Activity.java
+++ app/src/main/java/com/example/test_login/Admin_edit_user_Activity.java
@@ -14,51 +14,45 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 
-import kotlin.jvm.internal.FunctionAdapter;
-
-public class Admin_Activity extends AppCompatActivity {
-
-
+public class Admin_edit_user_Activity extends AppCompatActivity {
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     RecyclerView recyclerView;
     FirestoreRecyclerAdapter adapter;
 
-    LayoutInflater inflater = getLayoutInflater();
-    View alertLayout = inflater.inflate(R.layout.member_data, null);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin);
+        setContentView(R.layout.activity_admin_edit_user);
 
+        Intent i = getIntent();
+        String username = i.getStringExtra("username");
 
-        recyclerView = findViewById(R.id.recyclerView);
-        Query query = db.collection("user");
+        recyclerView = findViewById(R.id.recyclerViewEditUser);
+        Query query = db.document(username).collection("user");
         FirestoreRecyclerOptions<show_member> options = new FirestoreRecyclerOptions.Builder<show_member>()
                 .setQuery(query, show_member.class)
                 .build();
 
-        adapter = new FirestoreRecyclerAdapter<show_member, Admin_Activity.Data_holder>(options) {
+        adapter = new FirestoreRecyclerAdapter<show_member, Admin_edit_user_Activity.Data_holder>(options) {
             @NonNull
             @Override
-            public Admin_Activity.Data_holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.member_data, parent, false);
+            public Admin_edit_user_Activity.Data_holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.member_data_edit, parent, false);
                 return new Data_holder(view);
             }
 
             @Override
-            protected void onBindViewHolder(@NonNull Admin_Activity.Data_holder holder, int position, @NonNull show_member model) {
+            protected void onBindViewHolder(@NonNull Admin_edit_user_Activity.Data_holder holder, int position, @NonNull show_member model) {
                 holder.username.setText(model.getUsername());
                 holder.password.setText(model.getPassword());
                 holder.email.setText(model.getEmail());
@@ -66,14 +60,6 @@ public class Admin_Activity extends AppCompatActivity {
                 holder.name.setText(model.getName());
                 holder.gender.setText(model.getGender());
                 holder.status.setText(model.getStatus());
-                Button edit = holder.itemView.findViewById(R.id.btn_update_PerUser);
-                edit.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent(Admin_Activity.this, register_page.class);
-                        startActivity(intent);
-                    }
-                });
 
             }
         };
@@ -81,8 +67,8 @@ public class Admin_Activity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
 
-    }
 
+    }
 
     @Override
     protected void onStart() {
@@ -96,10 +82,9 @@ public class Admin_Activity extends AppCompatActivity {
         adapter.stopListening();
     }
 
-    public class Data_holder extends RecyclerView.ViewHolder {
+    private class Data_holder extends RecyclerView.ViewHolder {
 
         TextView username, password, email, phone, name, gender,status;
-        Button edit;
 
         public Data_holder(@NonNull View itemView) {
             super(itemView);
@@ -110,13 +95,11 @@ public class Admin_Activity extends AppCompatActivity {
             name =  itemView.findViewById(R.id.card_name);
             gender = itemView.findViewById(R.id.card_gender);
             status = itemView.findViewById(R.id.card_status);
-            edit = itemView.findViewById(R.id.btn_update_PerUser);
 
         }
     }
 
-
-    /*//
+    //
     void ald() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         LayoutInflater inflater = getLayoutInflater();
@@ -151,11 +134,7 @@ public class Admin_Activity extends AppCompatActivity {
 
             }
         });
-
-
-
-
-
         //test
-    }*/
+    }
+
 }
