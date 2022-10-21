86f243f4a05c1e1e536fd3c27da999f521151032
intania <nui-nui-nui@windowslive.com>
2022-10-21 15:01:23 +0700

    เพิ่ม FN. ในหน้า member_data.xml ให้มีปุ่ม Lock Unlock delete

--- app/src/main/java/com/example/test_login/Admin_Activity.java
+++ app/src/main/java/com/example/test_login/Admin_Activity.java
@@ -20,6 +20,7 @@ import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.auth.User;
@@ -32,6 +33,8 @@ public class Admin_Activity extends AppCompatActivity {
 
     RecyclerView recyclerView;
     FirestoreRecyclerAdapter adapter;
+
+    Button  lock, unlock, delete, update, logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,10 +77,26 @@ public class Admin_Activity extends AppCompatActivity {
                 //Convert status from 1 to Active and 0 to Inactive
                 if(model.getStatus().equals("1")) {
                     holder.status.setText("Active");
+
+                    //Set button btn_Unlock to false//
+                    Button btn_Unlock = holder.itemView.findViewById(R.id.btn_Unlock_user);
+                    btn_Unlock.setEnabled(false);
+
+                    //Set button btn_Lock to true//
+                    Button btn_Lock = holder.itemView.findViewById(R.id.btn_Lock_user);
+                    btn_Lock.setEnabled(true);
                 } else {
                     holder.status.setText("Inactive");
-                    //If status is Inactive, TextView will be red
+                    //If status is Inactive, TextView will be red//
                     holder.status.setTextColor(getResources().getColor(R.color.red));
+
+                    //Set button btn_Unlock to true//
+                    Button btn_Unlock = holder.itemView.findViewById(R.id.btn_Unlock_user);
+                    btn_Unlock.setEnabled(true);
+
+                    //Set button btn_Lock to false//
+                    Button btn_Lock = holder.itemView.findViewById(R.id.btn_Lock_user);
+                    btn_Lock.setEnabled(false);
                 }
             }
         };
@@ -103,7 +122,6 @@ public class Admin_Activity extends AppCompatActivity {
     public class Data_holder extends RecyclerView.ViewHolder {
 
         TextView username, password, email, phone, name, gender,status;
-        User user;
 
         public Data_holder(@NonNull View itemView) {
             super(itemView);
@@ -115,64 +133,44 @@ public class Admin_Activity extends AppCompatActivity {
             gender = itemView.findViewById(R.id.card_gender);
             status = itemView.findViewById(R.id.card_status);
 
+            //Unlock user//
+            itemView.findViewById(R.id.btn_Unlock_user).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    DocumentReference docRefUpdate = db.collection("user").document(username.getText().toString()); //Get username from TextView
+                    docRefUpdate.update("status","1"); //Update status to 1 or Active
+                    //Toast.makeText(Admin_Activity.this, "User is now Active", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            //Lock user
+            itemView.findViewById(R.id.btn_Lock_user).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    DocumentReference docRefUpdate = db.collection("user").document(username.getText().toString()); //Get username from TextView
+                    docRefUpdate.update("status","0"); //Update status to 1 or Active
+                    //Toast.makeText(Admin_Activity.this, "User is now LOCKED!!!", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            //Update user data
             itemView.findViewById(R.id.btn_update_PerUser).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     Intent intent = new Intent(Admin_Activity.this, Admin_edit_user_Activity.class);
                     intent.putExtra("username", username.getText().toString());
                     startActivity(intent);
+                }
+            });
 
-                    //Toast.makeText(Admin_Activity.this, "R", Toast.LENGTH_SHORT).show();
+            //Delete user
+            itemView.findViewById(R.id.btn_delete_PerUser).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    DocumentReference documentReference = db.collection("user").document(username.getText().toString());
+                    documentReference.delete();
                 }
             });
         }
-    }
-
-
-
-
-
-
-    //
-    void ald() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        LayoutInflater inflater = getLayoutInflater();
-        View view = inflater.inflate(R.layout.member_data_edit, null);
-        builder.setView(view);
-
-        EditText UserEdit = (EditText) findViewById(R.id.card_edit_username);
-        EditText PassEdit = (EditText) findViewById(R.id.card_edit_password);
-        EditText EmailEdit = (EditText) findViewById(R.id.card_edit_email);
-        EditText PhoneEdit = (EditText) findViewById(R.id.card_edit_phone);
-        EditText NameEdit = (EditText) findViewById(R.id.card_edit_name);
-        EditText Gender = (EditText) findViewById(R.id.card_edit_gender);
-
-        String UserEdit1 = UserEdit.getText().toString();
-        String PassEdit1 = PassEdit.getText().toString();
-        String EmailEdit1 = EmailEdit.getText().toString();
-        String PhoneEdit1 = PhoneEdit.getText().toString();
-        String NameEdit1 = NameEdit.getText().toString();
-
-
-        Button update = view.findViewById(R.id.btn_update55);
-        update.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseFirestore db = FirebaseFirestore.getInstance();
-                DocumentReference docRefUpdate = db.collection("user").document();
-                docRefUpdate.update("username", UserEdit1);
-                docRefUpdate.update("password", PassEdit1);
-                docRefUpdate.update("email", EmailEdit1);
-                docRefUpdate.update("phone", PhoneEdit1);
-                docRefUpdate.update("name", NameEdit1);
-
-            }
-        });
-
-
-
-
-
-        //test
     }
 }
